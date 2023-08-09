746d36ca7cf3acb4e4499d01aa1e0711cfb6deb8
nooreealotibi <nooreealotibi@gmail.com>
2023-08-10 01:00:08 +0300

    Initial Commit

---
+++ RedFlag1/RedFlag1App.swift
@@ -0,0 +1,17 @@
+//
+//  RedFlag1App.swift
+//  RedFlag1
+//
+//  Created by noor alotibi on 23/01/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct RedFlag1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
